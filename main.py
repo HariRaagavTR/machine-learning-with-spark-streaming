bb209d9114d8a3ad89ee89c7586fdc8d499a5683
HariRaagavTR <harirams01@gmail.com>
2021-12-04 17:37:52 +0530

    Testing Spark Streaming.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+
+import sys
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+sparkContext = SparkContext.getOrCreate()
+sparkStreamingContext = StreamingContext(sparkContext, 1)
+
+# TCP_IP = sys.argv[1]
+# TCP_PORT = int(sys.argv[2])
+
+TCP_IP = 'localhost'
+TCP_PORT = 6100
+
+dataStream = sparkStreamingContext.socketTextStream(TCP_IP, TCP_PORT)
+dataStream.pprint()
+
+sparkStreamingContext.start()
+sparkStreamingContext.awaitTermination()
