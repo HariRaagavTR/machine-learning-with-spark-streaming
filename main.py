92e494ac2a089ff3046ba2825f6a5e940912bfaf
HariRaagavTR <harirams01@gmail.com>
2022-01-05 17:16:56 +0530

    Modified main.py

--- main.py
+++ main.py
@@ -1,48 +1,58 @@
 #!/usr/bin/env python3
-from pyspark.sql import SparkSession
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from pyspark.streaming import StreamingContext
+import numpy as np
+import json
+
+def convertBatchToArray(batch):
+    """
+    Function that converts every batch into a numpy array which is used for further analysis.
+    Args:
+        batch (pyspark.rdd.RDD): A batch read from the spark stream.
+    Returns:
+        numpy.array: The batch data represented as a numpy array.
+    """
+    try:
+        data = json.loads(batch.collect()[0])
+        values = []
+        for index in data:
+            record = data[index]
+            image = np.asarray(record['image'])
+            label = record['label']
+            values.append(np.asarray([image, label]))
+        return np.asarray(values)
+    except:
+        print('Error: Empty or Invalid Batch Received.')
+        return np.asarray([])
+    
+def processBatch(batch):
+    """
+    Function that does the following:
+    1. Convert received batch (training data) into a numpy array.
+    2. Preprocess the numpy array.
+    3. Incrementally trains 3 different models using the preprocessed data.
+
+    Args:
+        batch (pyspark.rdd.RDD): A batch read from the spark stream.
+    Returns:
+        N/A.
+    """
+    if not batch.isEmpty():
+        values = convertBatchToArray(batch)
+        print(values)
 
 TCP_IP = 'localhost'
 TCP_PORT = 6100
-BATCH_SIZE = 10
 
-spark = SparkSession \
-    .builder \
-    .appName("Image Classifier") \
-    .getOrCreate()
+sparkContext = SparkContext("local[*]")
+sparkStreamingContext = StreamingContext(sparkContext, 1)
+sparkSQLContext = SQLContext(sparkContext)
 
-dfFromStream = spark.readStream \
-      .format("socket") \
-      .option("host", TCP_IP) \
-      .option("port", TCP_PORT) \
-      .load()
+dataStream = sparkStreamingContext \
+    .socketTextStream(TCP_IP, TCP_PORT) \
+    .foreachRDD(processBatch)
 
-# schema =  StructType([
-#     StructField("img", StringType(), True),
-#     StructField("label", StringType(), True)
-# ])
-
-# convertedDataFrame = dfFromStream \
-#     .withColumn("jsonData",from_json(col("value"), schema)) \
-#     .select("jsonData.*")
-    
-query = dfFromStream.writeStream \
-    .outputMode("append") \
-    .format("console") \
-    .start()
-    
-query.awaitTermination()
-
-"""
-"0": {
-    "img": [[[]]]
-    "label": "className"
-},
-"1": {
-    "img": [[[]]]
-    "label": "className"
-},
-"2": {
-    "img": [[[]]]
-    "label": "className"
-},
-"""
+sparkStreamingContext.start()
+sparkStreamingContext.awaitTermination(1000)
+sparkStreamingContext.stop()
